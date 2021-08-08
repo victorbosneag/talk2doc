1d2acade72ebd9d35738975dbdae3d9f32371dbb
Nihal Menon <31966964+nihalmenon@users.noreply.github.com>
2021-08-07 21:52:54 -0500

    Add files via upload

--- src/patient.js
+++ src/patient.js
@@ -1,12 +1,18 @@
-const axios = require('./axios');
+// const axios = require('./axios');
+
+var symptomsDict = {
+
+}
+
+
+
 
 console.log('test');
 //const form = document.getElementById('dailyCheckInForm');
 //const symptoms = document.getElementById('symptoms');
 //const otherInfo = document.getElementById('otherInfo');
 $(document).ready(function() {
-    $('submit-checkIn-btn').on('click', function(e) {
-        console.log('submit clicked');
+    $('#submit-checkIn-btn').on('click', function(e) {
         /*
         const sendInfo = async () => {
             const promsie = await axios({
@@ -16,5 +22,20 @@ $(document).ready(function() {
         }
         sendInfo();
         */
-    })
-})
+        var symptoms = ["Fever/chills", "Cough", "Loss of taste/smell", "Shortness of breath", "Fatigue", "Muscle/body aches", "Headaches", "Sore throat", "Congestion/runny nose", "Nausea", "Diarrhea"]
+        var severities = []
+        
+        for(var i = 0; i < symptoms.length; i++){
+            severities.push(0);
+        }
+    
+        
+        $("input.severity").each(function(i, obj) {
+            if($(this).val() != ""){
+                severities[i] = $(this).val();
+            }
+        });
+
+        console.log(severities.toString());
+    });
+});
